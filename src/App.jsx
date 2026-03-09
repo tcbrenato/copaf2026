d50ff16b4f72a2376a57eead1c9d34ec31669483
tcbrenato <renatotchobo0@gmail.com>
2026-03-09 15:53:57 +0100

    feat: ajout route /admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -6,19 +6,32 @@ import Programme from './components/Programme'
 import Modules from './components/Modules'
 import Inscription from './components/Inscription'
 import Footer from './components/Footer'
+import AdminDashboard from './components/AdminDashboard'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Programme />
-        <Modules />
-        <Inscription />
-        <Footer />
-      </main>
+      <Routes>
+
+        {/* Site principal */}
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <main>
+              <Hero />
+              <About />
+              <Programme />
+              <Modules />
+              <Inscription />
+            </main>
+            <Footer />
+          </>
+        } />
+
+        {/* Page admin — accessible sur /admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+      </Routes>
     </Router>
   )
 }
