d7d141417d650b2e2d29f5e30653a74d39fdd28d
tcbrenato <renatotchobo0@gmail.com>
2026-04-12 00:58:23 +0100

    fix: correction URL Supabase vers nouveau projet pdtohaxbsgpxccopgnmd

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://gorkgxiqknbpuvaocqlo.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvcmtneGlxa25icHV2YW9jcWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4OTg2NTQsImV4cCI6MjA5MTQ3NDY1NH0.DshZyuMReq6e4GfNoJENK9ZSK6QvUrRfilbz6LkFvXM'
+const SUPABASE_URL = 'https://pdtohaxbsgpxccopgnmd.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdG9oYXhic2dweGNjb3Bnbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzI4MDMsImV4cCI6MjA5MTUwODgwM30.Nwdxq1V63LTE9duI30KN9c6JQ_OPhyU0_ERwobnUGGk'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
